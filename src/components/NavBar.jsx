306598045bdb2297478a16341b378c9528ba9b2b
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-07-20 13:58:59 +0530

    see all my connection req

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -54,6 +54,11 @@ const NavBar = () => {
         </li>
         <li>
         <Link to="/connections">Connections</Link> </li>
+
+           <li>
+        <Link to="/requests">Requests </Link> 
+        </li>
+
         <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
